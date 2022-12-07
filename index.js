c8995dd5c30b5c623826891c0f2f8a8070b7a325
David Martos Ruiz <92975283+davidmartosruiz@users.noreply.github.com>
2022-12-07 10:27:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function constructStatusStream(key, url, uptimeData) {
   const color = getColor(lastSet);
 
   const container = templatize("statusContainerTemplate", {
-    title: key.replace('_', ' ');,
+    title: key.replace('_', ' '),
     url: url,
     color: color,
     status: getStatusText(color),
